eaf757f2ca9cccd13f1c8ba04d262bed843162f9
GleenMontiel <65775735+GleenMontiel@users.noreply.github.com>
2020-12-03 17:31:54 +0100

    First Commit

---
+++ Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(231, 18);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(949, 653);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "View Processes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 78);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Process info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 143);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Close process";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 205);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Kill process";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(18, 271);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Run App";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 371);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Ejercicio2/Ejercicio2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+      
+        Process[] processes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            processes = Process.GetProcesses();
+            
+          
+        }
+
+        public String LongitudCadena(string cad, int longitud)
+        {
+            return cad.Length > longitud? cad.Substring(0,12)+"...":cad;
+        }
+
+        public void ViewProcesses() 
+        {
+            string processName = "";
+            string title = "";
+            const string FORMAT = "{0,-8}{1,-20}{2,-20}\r\n";
+            textBox1.Text = string.Format(FORMAT, "PID", "NAME", "TITLE");
+            foreach (Process p in processes)
+            {
+                processName = LongitudCadena(p.ProcessName, 15);
+                title = LongitudCadena(p.MainWindowTitle, 15);
+                textBox1.Text += string.Format(FORMAT, p.Id, processName, title);
+
+            }
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ViewProcesses();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               
+               
+                int pid = int.Parse(textBox2.Text);
+                textBox1.Clear();
+                Process p = Process.GetProcessById(pid);
+
+                string processName = LongitudCadena(p.ProcessName, 15);
+                string title = LongitudCadena(p.MainWindowTitle, 15);
+
+
+
+                const string FORMAT = "{0,-8}{1,-20}{2,-20}\r\n";
+                const string FORMAT2 = "{0,-8}{1,-20}\r\n";
+                const string FORMAT3 = "{0,-20}{1,-35}\r\n";
+
+                textBox1.Text = string.Format(FORMAT, "PID", "NAME", "TITLE");
+                textBox1.Text += string.Format(FORMAT, p.Id, processName, title);
+
+                textBox1.Text += String.Format("\r\n{0,-8}{1,-15}{2,-20}\r\n", "ID", "PRIORITY", "STATE");
+                ProcessThreadCollection pt = p.Threads;
+                foreach (ProcessThread t in pt)
+                {
+                    textBox1.Text += String.Format("{0,-8}{1,-15}{2,-20}\r\n", t.Id, t.PriorityLevel, t.ThreadState);
+                }
+
+                ProcessModuleCollection pm = p.Modules;
+
+
+                textBox1.Text += string.Format("\r\n"+FORMAT3, "MODULE NAME", "FILE NAME");
+                foreach (ProcessModule module in pm)
+                {
+                    string moduleName = LongitudCadena(module.ModuleName, 15);
+                    string fileName = LongitudCadena(module.FileName, 15);
+
+                    textBox1.Text += string.Format(FORMAT3, moduleName, fileName);
+                }
+
+
+
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                Console.WriteLine("SORRY");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Introduce un dato válido");
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pid = int.Parse(textBox2.Text);
+                textBox1.Clear();
+                Process p = Process.GetProcessById(pid);
+
+                DialogResult r = MessageBox.Show("¿Seguro quieres cerrar?", "Procesos", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                switch (r)
+                {
+
+                    case DialogResult.OK:
+
+                        p.CloseMainWindow();
+                        break;
+                    case DialogResult.Cancel:
+                        MessageBox.Show("No se cerro nada", "Procesos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                }
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Introduce un dato válido");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pid = int.Parse(textBox2.Text);
+                textBox1.Clear();
+                Process p = Process.GetProcessById(pid);
+
+                DialogResult r = MessageBox.Show("¿Seguro quieres cerrar?", "Procesos", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                switch (r)
+                {
+
+                    case DialogResult.OK:
+
+                        p.Kill();
+                        break;
+                    case DialogResult.Cancel:
+                        MessageBox.Show("No se cerro nada", "Procesos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Introduce un dato válido");
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string program = textBox2.Text;
+                Process p = Process.Start(program);
+            }
+            catch
+            {
+                Console.WriteLine("Introduce un dato válido");
+            }
+        }
+    }
+}
